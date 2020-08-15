eea511f58bdcb260469462ff6c73ecd979b2cf42
Jorge Octavio <jorgeo80@gmail.com>
2020-08-15 18:48:40 -0500

    [ADD] node-06

---
+++ node-06/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+
+app.get('/name', function (req, res) {
+  // res.send('Hola Mundo')
+  let salida = {
+    nombre: 'karen',
+    edad: 99,
+    url: req.url
+  }
+  res.send(salida)
+})
+ 
+app.listen(3000, () => {
+  console.log('Escuchando peticiones en el puerto 3000')
+})
